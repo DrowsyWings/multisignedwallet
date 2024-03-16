2572ee287795ddd8c0875a4edc88fb9e936bf77f
DrowsyWings <114069481+DrowsyWings@users.noreply.github.com>
2024-03-16 13:48:17 +0530

    Transfer And Approval

--- src/multiSignWallet_frontend/src/App.jsx
+++ src/multiSignWallet_frontend/src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Approver from "./components/Approver";
 import Transfer from "./components/Transfer";
-import './index.scss';
+import TransferApproval from "./components/TransferApproval";
+import "./index.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-       <div className="SideBySideContainer">
-      <Approver />
-      <Transfer />
-       </div>
-
+      <div className="SideBySideContainer">
+        <Approver />
+        <Transfer />
+      </div>
+      <TransferApproval />
     </div>
   );
 }
---
+++ src/multiSignWallet_frontend/src/components/TransferApproval.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/TransferApproval.css";
+
+const TransferApproval = () => {
+  const getAmount = () => {
+    //  to get the amount
+  };
+
+  const getAddress = () => {
+    //  logic to get the address
+  };
+
+  const increaseCounter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const increaseCounter = () => {
+      setCounter(counter + 1);
+    };
+  };
+
+  const isSent = () => {
+    // logic to determine if it is sent
+  };
+
+  const renderRows = () => {
+    //  logic to render rows dynamically
+  };
+
+  return (
+    <div className="transfer-approval">
+      <h2 className="transfer-approval-heading">Transfer-Approvals</h2>
+      <table className="transfer-approval-table">
+        <thead>
+          <tr>
+            <th>ID (Serial No.)</th>
+            <th>Amount</th>
+            <th>Address</th>
+            <th>Approvals</th>
+            <th>Sent</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>{getAmount()}</td>
+            <td>{getAddress()}</td>
+            <td>
+              <button onClick={increaseCounter}>Approve</button>
+            </td>
+            <td>{isSent() ? "Yes" : "No"}</td>
+          </tr>
+          {renderRows()}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TransferApproval;
